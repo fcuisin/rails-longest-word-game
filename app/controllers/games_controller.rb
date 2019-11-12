34f831f8102d145f1167504f50b9b773d803cdb0
Ferdinand Cuisin <fcuisin@gmail.com>
2019-11-11 22:01:00 -0500

    Added parsing method. Refactor generating words method with vowels. Display final result

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,8 @@
+require "open-uri"
+
 class GamesController < ApplicationController
+  VOWELS = %w(A E I O U Y)
+
   def new
     @letters = generate_grid
   end
@@ -6,7 +10,10 @@ class GamesController < ApplicationController
   def score
     @word = params[:answer]
     @answer_splitted = params[:letters].split(' ')
-    @result = included?(@word, @answer_splitted)
+    @include = included?(@word, @answer_splitted)
+    @en_word = en_word?(@word)
+
+    result
   end
 
   private
@@ -14,13 +21,34 @@ class GamesController < ApplicationController
   def generate_grid
     @letters = []
     array = ('A'..'Z').to_a
-    10.times do
-      @letters << array.sample
+    5.times do
+      @letters << VOWELS.sample
+    end
+    5.times do
+      @letters << (array - VOWELS).sample
     end
     @letters
   end
 
   def included?(word, grid)
-    word.split('').all? { |letter| grid.include? letter }
+    word.upcase.split('').all? { |letter| grid.include?(letter) }
+  end
+
+  def en_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word.downcase}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+
+  def result
+    if @include == true
+      if @en_word == true
+        @result = "Yay"
+      else
+        @result = "Good shot but try again!"
+      end
+    else
+      @result = "Letters out of the grid"
+    end
   end
 end
